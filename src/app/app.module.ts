66eab317a533a3d226c55a7122bf53ba31a14996
Ingrid Midori <ingrid.midori@brunsker.com.br>
2024-04-24 20:36:42 -0300

    fix: remove useless components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomePageModule } from './core/home-page/home-page.module';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HomePageModule, AppRoutingModule],
+  imports: [BrowserModule, RouterModule, HomePageModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/core/home-page/home-page-routing.module.ts
+++ src/app/core/home-page/home-page-routing.module.ts
@@ -1,23 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './ui/home-page.component';
-import { Child1PageComponent } from './pages/child1-page/child1-page.component';
-import { Child2PageComponent } from './pages/child2-page/child2-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePageComponent,
-    children: [
-      {
-        path: 'child1',
-        component: Child1PageComponent,
-      },
-      {
-        path: 'child2',
-        component: Child2PageComponent,
-      },
-    ],
+    component: HomePageComponent
   },
 ];
 
-export const HomePageRoutingModule = RouterModule.forChild(routes);
+export const HomePageRoutingModule = RouterModule.forRoot(routes);
